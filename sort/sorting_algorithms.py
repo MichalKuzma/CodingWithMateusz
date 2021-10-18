6976c92091619650008807255fb4b35d105ece67
Unknown <michal.a.kuzma@gmail.com>
2021-10-18 18:17:09 +0200

    Boilerplate for sort algorithms practice

---
+++ sort/sorting_algorithms.py
@@ -0,0 +1,23 @@
+import random
+
+def selection_sort(t):
+    
+    
+    return t
+
+def bubble_sort(t):
+    
+    
+    return t
+
+def merge_sort(t):
+
+
+    return t
+
+x = random.sample(range(0, 100), 10)
+print('Input List:\t', x)
+
+print('Selection Sort:\t', selection_sort(x))
+print('Bubble Sort:\t', bubble_sort(x))
+print('Merge Sort:\t', merge_sort(x))
