c13ee2938a155bbb826e1129eff7f492ac6e54f3
Mateusz <mateusz223355@gmail.com>
2021-10-18 20:39:58 +0200

    bubble_sort

--- sort/sorting_algorithms.py
+++ sort/sorting_algorithms.py
@@ -1,11 +1,29 @@
 import random
 
 def selection_sort(t):
-    
+    i = 0
+    for i in range(len(t) - 1):
+        min_index = i
+        j = i + 1
+        for j in range(len(t)):
+            if t[min_index] > t[j]:
+                min_index = j
+                j += 1
+        t[i] , t[min_index] = t[min_index] , t[i]
+        i += 1
+
     
     return t
 
 def bubble_sort(t):
+    i = 0
+    for i in range(len(t) - 1):
+        j = 0
+        for j in range(len(t) - 1):
+            if t[j] > t[j + 1]:
+                t[j] , t[j + 1] = t[j +1] , t[j]
+            j += 1
+        i += 1
     
     
     return t
